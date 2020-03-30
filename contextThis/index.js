252262ca235f7bff1d3d5e789fb0cd0e5261c5c8
John Doe <vperedz@gmail.com>
2020-03-30 12:17:10 +0300

    add

--- contextThis/index.js
+++ contextThis/index.js
@@ -5,14 +5,14 @@ export let user = {
         return (`${this.firstName} ${this.lastName}`);
     },
     setFullName(fullName) {
-        const [firstName, lastName] = fullName.split(' ');
+        // const [firstName, lastName] = fullName.split(' ');
         
-        //  this.firstName = fullName.split(' ')[0]; 
-        //  this.lastName = fullName.split(' ')[1];  
+         this.firstName = fullName.split(' ')[0]; 
+         this.lastName = fullName.split(' ')[1];  
         //  console.log(fullName.split(' '));
 
-         this.firstName = firstName;
-         this.lastName = lastName
+        //  this.firstName = firstName;
+        //  this.lastName = lastName
     }
 };
 
