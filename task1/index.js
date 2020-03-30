30b2572793c19ca5e71b7a984ee798a4b1e5306d
John Doe <vperedz@gmail.com>
2020-03-30 12:30:16 +0300

    add

--- task1/index.js
+++ task1/index.js
@@ -1,24 +1,20 @@
 export let user = {
-    firstName: 'John',
-    lastName: 'Doe',
+    firstName: 'Tom',
+    lastName: 'Ford',
     getFullName() {
         return (`${this.firstName} ${this.lastName}`);
     },
     
-    setFullName(value) {
-        // const [firstName, lastName] = fullName.split(' ');
+    setFullName(fullName) {
+        const [firstName, lastName] = fullName.split(' ');
         
-        let split = value.split(' ');
-        this.firstName = split[0];
-        this.lastName = split[1];
-        
+         this.firstName = firstName;
+         this.lastName = lastName
+             
         
         //  this.firstName = fullName.split(' ')[0]; 
         //  this.lastName = fullName.split(' ')[1];  
         //  console.log(fullName.split(' '));
-
-        //  this.firstName = firstName;
-        //  this.lastName = lastName
     }
 };
 
