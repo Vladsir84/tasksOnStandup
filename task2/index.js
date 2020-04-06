4a115ce3e253d6d036b46d91c7dd65233d390362
John Doe <vperedz@gmail.com>
2020-04-06 13:29:14 +0300

    add

--- task2/index.js
+++ task2/index.js
@@ -4,38 +4,19 @@ const dayTransactions = [
     {currency: 'EUR', amount: 90, operation: 'sell'},
 ];
 
-function getTotalBalance(transactionsList) {
-   const transformedTransaction = transactionsList.map(function(transactionObj) {
-      const key = transactionObj.currency;
-      const value = transactionObj.amount;
-    const obj = {
-    [key] : transactionObj.operation === 'sell' ?  -value : value
-    };
-    return obj;
-   });
-  
-   const result = transformedTransaction.reduce(function(acc, transactionObj) {
-      console.log(transactionObj);
-    //   {USD : -60}
-    //   {EUR : 160}
-    //   {EUR : -90}
-      Object.assign(acc, transactionObj);
-      
-      return acc
-    }, {});
-    return result;
+function getTotalBalance(transactions) {
+  const result = transactions.reduce(function(acc, transaction) {
+      const { currency, amount, operation } = transaction;
+      const oldAmount = acc[currency] || 0;
+      const newAmount =
+          operation === 'sell' ? oldAmount - amount : oldAmount + amount;
+      return {
+          ...acc,
+          [currency]: newAmount,
+      };
+  }, {});
+  return result;
 }
-
-// const reduces = {
-//     USD: function(state, item) {
-//         return state.rubles += item.price;
-//       },
-//     EUR: function(state, item) {
-//         return state.dollars += item.price 
-//       },
-// }
-
-// const result1 = e()
 
 const result = getTotalBalance(dayTransactions);
 console.log(result);
