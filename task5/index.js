364704b128aaacb8c9d53cb9871b8987b6443c5a
John Doe <vperedz@gmail.com>
2020-04-13 12:34:07 +0300

    add

---
+++ task5/index.js
@@ -0,0 +1,6 @@
+export const setButton = (buttonText) => {
+  let bodyElem = document.querySelector('body');
+  bodyElem.innerHTML = '<button>button text</button>'
+//   bodyElem.textContent = '<button>button text</button>'
+  buttonText = text;
+}
