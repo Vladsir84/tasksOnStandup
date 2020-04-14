d45337c5c84e6e4b6fcacdac50cf6e67172f8962
John Doe <vperedz@gmail.com>
2020-04-14 13:41:25 +0300

    add

---
+++ task7/index.js
@@ -0,0 +1,12 @@
+const listItems = document.querySelectorAll('.list-item');
+const list = document.querySelector('.list');
+
+function handleClick(event) {
+    const isListItem = Boolean(event.target.closest('.list-item'));
+    
+    if(isListItem) {
+        console.log(event.target.textContent);
+    }
+}
+
+list.addEventListener('click', handleClick);
