cf48c5a45d5e4cb05705fe8c436eaf8a7e33d03e
John Doe <vperedz@gmail.com>
2020-05-12 13:11:15 +0300

    add

--- task12/src/Pagination.jsx
+++ task12/src/Pagination.jsx
@@ -1,37 +1,33 @@
 import React from 'react';
 
-const Pagination = (props) => {
-    const { currentPage, goPrev, goNext, totalItems, itemsPerPage } = props;
-    const isPrevPageAvailable = currentPage > 0;
-    const isNextPageAvailable = currentPage < Math.ceil(totalItems / itemsPerPage) - 1;
-
+const Pagination = ({
+    goPrev,
+    goNext,
+    currentPage,
+    totalItems,
+    itemsPerPage,
+}) => {
+    const prevPageAvailable = currentPage > 0;
+    const nextPageAvailable =
+        currentPage < Math.ceil(totalItems / itemsPerPage) - 1;
     return (
-
         <div className="pagination">
             <button
-                onClick={goPrev}
                 className="btn"
-                type="button"
-                disabled={!isPrevPageAvailable}
-                >
-                
-                {isPrevPageAvailable && '←'}
-            
+                onClick={goPrev}
+                disabled={!prevPageAvailable}
+            >
+                {prevPageAvailable && '←'}
             </button>
             <span className="pagination__page">{currentPage + 1}</span>
             <button
-                onClick={goNext}
                 className="btn"
-                disabled={!isNextPageAvailable}
-                type="button"
-                >
-                {isNextPageAvailable && '→'}
+                onClick={goNext}
+                disabled={!nextPageAvailable}
+            >
+                {nextPageAvailable && '→'}
             </button>
         </div>
     );
 };
-
 export default Pagination;
-
-
-
