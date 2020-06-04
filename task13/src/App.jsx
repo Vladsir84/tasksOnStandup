43ee6128828e5c662f8a7bf6311000e5f3b49392
John Doe <vperedz@gmail.com>
2020-06-04 12:33:27 +0300

    add

---
+++ task13/src/App.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Route, Link, Switch } from 'react-router-dom';
+import Users from './Users';
+import Home from './Home';
+
+const App = () => {
+    return (
+        <div className="page">
+            <ul className="navigation">
+                <li className="navigation__item">
+                    <Link to="/users">Users</Link>
+
+                </li>
+                <li className="navigation__item">
+                    <Link to="/">Home</Link>
+                </li>
+            </ul>
+            <Switch>
+                <Route exact path="/">
+                    <Home />
+                </Route>
+                <Route path="/users" component={Users} />
+                   
+                {/* </Route> */}
+            </Switch>
+        </div>
+    );
+};
+
+
+export default App;
---
+++ task13/src/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import './index.scss';
+import App from './App';
+
+const rootElement = document.querySelector('#root');
+
+
+ReactDOM.render(
+    <BrowserRouter >
+        <App />
+    </BrowserRouter>, rootElement);
