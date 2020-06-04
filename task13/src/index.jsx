759ba55356504eebc2bac976dd1a25acc1d431e0
John Doe <vperedz@gmail.com>
2020-06-04 14:57:49 +0300

    add

--- task13/src/index.jsx
+++ task13/src/index.jsx
@@ -1,13 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import { BrowserRouter } from 'react-router-dom';
 import './index.scss';
 import App from './App';
 
 const rootElement = document.querySelector('#root');
 
 
-ReactDOM.render(
-    
-        <App />
-    , rootElement);
+ReactDOM.render(<App />, rootElement);
